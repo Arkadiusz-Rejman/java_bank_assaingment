b911981ccc80fe87cec55d30f079c1d9e921b81a
Arkadiusz Rejman <tylko213@gmail.com>
2024-01-12 18:33:43 +0100

    added sql connectivity - able to login 2 new classes User and mySQL_class build working :)

---
+++ src/main/java/org/example/java_bank_app/mySQL_class.java
@@ -0,0 +1,38 @@
+package org.example.java_bank_app;
+
+import java.math.BigDecimal;
+import java.sql.*;
+
+public class mySQL_class{
+    private  static final String DB_url = "jdbc:mysql://127.0.0.1:3306/bankapp";
+    private static final String DB_username = "root";
+    private static final String DB_password = "java1234";
+
+
+    public static User validateLogin(String username, String password){
+        try{
+            Connection connection = DriverManager.getConnection(DB_url,DB_username,DB_password);
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM user WHERE username = ? AND userpassword = ?"
+            );
+
+            preparedStatement.setString(1,username);
+            preparedStatement.setString(2,password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+            if(resultSet.next()){
+                int userId = resultSet.getInt("id");
+                BigDecimal currentBalance = resultSet.getBigDecimal("balance");
+                return new User(userId,username,password,currentBalance);
+            }
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+}
