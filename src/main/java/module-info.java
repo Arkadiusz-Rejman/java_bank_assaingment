418db0f0359aad2154d95880e873f27aacb41eb2
j.wili <jw302419@student.polsl.pl>
2024-01-15 23:43:02 +0100

    dsa

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.java_bank_app {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.java;
 
 
     opens org.example.java_bank_app to javafx.fxml;
