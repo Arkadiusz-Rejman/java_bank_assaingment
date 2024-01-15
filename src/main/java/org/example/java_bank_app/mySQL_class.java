7c40ebb7d314905ba972a9e9db5bacde050207dd
j.wili <jw302419@student.polsl.pl>
2024-01-15 13:18:20 +0100

    dsa

--- src/main/java/org/example/java_bank_app/mySQL_class.java
+++ src/main/java/org/example/java_bank_app/mySQL_class.java
@@ -4,13 +4,17 @@ import java.math.BigDecimal;
 import java.sql.*;
 
 public class mySQL_class{
-    private  static final String DB_url = "jdbc:mysql://127.0.0.1:3306/bankapp";
+    private static final String DB_url = "jdbc:mysql://127.0.0.1:3306/bankapp";
     private static final String DB_username = "root";
     private static final String DB_password = "java1234";
 
 
+
+
+
     public static User validateLogin(String username, String password){
         try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(DB_url,DB_username,DB_password);
             PreparedStatement preparedStatement = connection.prepareStatement(
                     "SELECT * FROM user WHERE username = ? AND userpassword = ?"
@@ -30,6 +34,8 @@ public class mySQL_class{
 
         }catch(SQLException e){
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
         return null;
 
