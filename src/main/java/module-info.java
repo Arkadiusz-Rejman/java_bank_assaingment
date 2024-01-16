ce306e8c0e2382f982d7c9580b0467fb31dd1c9b
j.wili <jw302419@student.polsl.pl>
2024-01-16 13:45:53 +0100

    dsa

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,12 @@ module org.example.java_bank_app {
 
     opens org.example.java_bank_app to javafx.fxml;
     exports org.example.java_bank_app;
+    exports org.example.java_bank_app.CurrencyPackage;
+    opens org.example.java_bank_app.CurrencyPackage to javafx.fxml;
+    exports org.example.java_bank_app.SQLPackage;
+    opens org.example.java_bank_app.SQLPackage to javafx.fxml;
+    exports org.example.java_bank_app.ControllersPackage;
+    opens org.example.java_bank_app.ControllersPackage to javafx.fxml;
+    exports org.example.java_bank_app.UserClassesPackage;
+    opens org.example.java_bank_app.UserClassesPackage to javafx.fxml;
 }
