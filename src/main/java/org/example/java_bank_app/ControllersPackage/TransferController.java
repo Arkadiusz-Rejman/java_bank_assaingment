12541528489bd6ef3a89b3b5ba9fc94bce3cc3b5
Arkadiusz Rejman <tylko213@gmail.com>
2024-01-23 00:19:44 +0100

    przesyłanie aktualnego walleta do klasy TrasnferController i ustawienie domyślnego portflea w choicebox na podstawie aktualnego w głównym menu :) - build działa

--- src/main/java/org/example/java_bank_app/ControllersPackage/TransferController.java
+++ src/main/java/org/example/java_bank_app/ControllersPackage/TransferController.java
@@ -1,8 +1,7 @@
 package org.example.java_bank_app.ControllersPackage;
 
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.beans.property.ObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
@@ -10,7 +9,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.example.java_bank_app.UserClassesPackage.User;
 import org.example.java_bank_app.UserClassesPackage.Wallet;
-import org.example.java_bank_app.SQLPackage.mySQL_class;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,26 +22,31 @@ public class TransferController implements Initializable {
     public ChoiceBox<Wallet> wallet_box;
     @FXML
     public Label balance_label;
+    ObjectProperty<Wallet> actuallWallet;
 
 
     User user;
+
+    public void passActuallWallet(ObjectProperty<Wallet> actuallWallet) {
+        this.actuallWallet = actuallWallet;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         Platform.runLater(() -> {
             wallet_box.setItems(user.getWallets());
-            wallet_box.setValue(wallet_box.getItems().getFirst());
+            wallet_box.setValue(actuallWallet.getValue());
             balance_label.setText("Current balance is: "+ wallet_box.getValue().getMoneyAmount()+" "+wallet_box.getValue().getCurrency().getCurrencyCode());
-
+            System.out.println(actuallWallet);
             wallet_box.valueProperty().addListener((observable, oldValue, newValue) -> balance_label.setText("Current balance is: "+ wallet_box.getValue().getMoneyAmount()+" "+wallet_box.getValue().getCurrency().getCurrencyCode()));
-
     });
     }
 
     public void passUser(User user){
         this.user = user;
-        System.out.println();
-
     }
+
 
 
 }
